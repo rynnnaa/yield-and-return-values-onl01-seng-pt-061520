9e0aa6f5a97ccea769eb6eac047e91d832f1e44e
Ryna Truong <rynatruong@outlook.com>
2020-06-22 16:57:32 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,7 +1,6 @@
 require 'pry'
 
 def hello(array)
-  binding.pry
   i = 0
   while i < array.length
     yield(array[i])
@@ -9,4 +8,5 @@ def hello(array)
   end
 end
 
-hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
+binding.pry
+hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
